ea48873f42686bd42e4c806fb1dd8d02b6980dd9
vishnupriya <priyareddy6720@gmail.com>
2023-07-06 17:24:50 +0530

    pdf and java files are ready

---
+++ b.java
@@ -0,0 +1,14 @@
+package factorial;
+
+public class fact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int fact=1,n=5;
+for(int i=1;i<=n;i++) {
+	fact=fact*i;
+}
+System.out.println(fact);
+	}
+
+}
